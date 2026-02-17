87a8ce7d55139899a29421f2000167d0a9b9e7dd
meng hor <piseth125125@gmail.com>
2026-02-17 20:39:36 +0700

    fix: color

--- mobile_year4/lib/colors.dart
+++ mobile_year4/lib/colors.dart
@@ -7,7 +7,8 @@ class AppColors {
   
   // 2. Backgrounds
   static const Color background = Color(0xFFF5F5F5); // grey[100]
-  static const Color cardBg = Colors.white;
+  static const Color cardBg = Color.fromARGB(255, 153, 139, 139);
+  static const Color lightGray = Color.fromARGB(255, 179, 173, 173);
 
   // 3. Status/Action Colors
   static const Color success = Colors.green;
@@ -18,4 +19,17 @@ class AppColors {
   static const Color textPrimary = Colors.black87;
   static const Color textSecondary = Colors.grey;
   static const Color textOnDark = Colors.white;
+  // 5. Soft Surfaces & Borders
+  static const Color border = Color(0xFFEEEEEE);      // For thin lines between items
+  static const Color shadow = Color(0x0D000000);      // Very light shadow (5% opacity)
+  
+  // 6. Light Action Backgrounds (Great for icon circles)
+  static Color accentLight = Colors.blue.withValues(alpha:0.1); 
+  static Color dangerLight = Colors.red.withValues(alpha:0.1);
+  static Color successLight = Colors.green.withValues(alpha:0.1);
+
+  // 7. Extra Text Shades
+  static const Color textHint = Color(0xFF9E9E9E);
+  static const Color textBlack = Color(0xFF000000);
+  
 }
--- mobile_year4/lib/screens/admin/profile/profile_view.dart
+++ mobile_year4/lib/screens/admin/profile/profile_view.dart
@@ -105,7 +105,7 @@ void _handleLogout() {
                   Container(
                     width: double.infinity,
                     decoration: const BoxDecoration(
-                      color: AppColors.primary,
+                      color: AppColors.lightGray,
                       borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
                     ),
                     padding: const EdgeInsets.only(bottom: 40, top: 20),
@@ -193,7 +193,7 @@ void _handleLogout() {
         borderRadius: BorderRadius.circular(15),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.03),
+            color: Colors.black.withValues(alpha: 0.03),
             blurRadius: 10,
             offset: const Offset(0, 4),
           )
